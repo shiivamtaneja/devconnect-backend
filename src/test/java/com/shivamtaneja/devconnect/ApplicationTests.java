40c4f608f3181c0cffc4d58908fc9cb60ae002b0
ShivamTaneja <shiivamtaneja@gmail.com>
2025-04-15 11:21:39 +0530

    first commit

---
+++ src/test/java/com/shivamtaneja/devconnect/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivamtaneja.devconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
